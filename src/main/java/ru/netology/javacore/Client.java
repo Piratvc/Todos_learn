81a81d8b84f606e93842e9d803fa61b1f2385d28
Piratvc <piratvc@gmail.com>
2022-05-22 21:12:39 +0400

    commit

--- src/main/java/ru/netology/javacore/Client.java
+++ src/main/java/ru/netology/javacore/Client.java
@@ -20,7 +20,7 @@ public class Client {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         try (
-                Socket socket = new Socket("localhost", 8080);
+                Socket socket = new Socket("localhost", 8989);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         ) {
